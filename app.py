04fb132264444677fc47f1ed611d11cb8042ba6a
aulia-fz <auliafitrizaimah@students.unnes.ac.id>
2025-06-04 13:52:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+import streamlit as st
+st.set_page_config(page_title="Proyek Data Mining", layout="centered")
+st.title("Proyek Data Mining")
 
+st.header("Perkenalan Kelompok")
+st.markdown("""Halo! Kami dari kelompok 1 Data Mining yang terdiri dari:
+1. Dayinta Ajeng Nariswari (4101400048)
+2. Firyal Daffa Nisrinna (4101422049)
+3. Aulia Fitri Za'imah (4101422053)
+4. Yarfak Rafiansya Saputra (4101422067)
+5. Ani Rahayu (2304030003)
+Kami sedang mengerjakan sebuah proyek yang menerapkan konsep-konsep data mining untuk analisis data nyata.
+""")
+
+st.header("Dosen Pengampu")
+st.markdown("""Mata Kuliah ini dibimbing oleh: Bapak M. Faris Al Hakim S.Pd., M.Cs.""")
+
+st.header("Fitur Aplikasi")
+st.markdown("""
+Aplikasi ini memiliki dua fitur yaitu:
+1. Data Gempa Bumi – Menampilkan grafik dan tabel interaktif dari dataset gempa bumi yang digunakan.
+2. Prediksi Gempa Bumi – Membersihkan dan mempersiapkan data untuk dianalisis dan prediksi kejadian gempa bumi.
+""")
+st.markdown("---")
+st.caption("Dibuat dengan ❤️ oleh Kelompok 1 Data Mining")
